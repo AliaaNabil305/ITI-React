ed8ae0b0d310888ba83164936a7556cca635beb0
Aliaa Nabil <114958840+AliaaNabil305@users.noreply.github.com>
2023-09-03 14:17:44 +0300

    Add files via upload

---
+++ task17/src/App.js
@@ -0,0 +1,17 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import Movies from "./Component/Movies";
+
+
+
+function App() {
+  return (
+    <div className="App">
+   <Movies></Movies>
+    
+    </div>
+   
+  );
+}
+
+export default App;
